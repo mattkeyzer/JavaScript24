aae3bb0f410e381e3438935d922d9b2e0e921c0f
Matt Keyzer <mattk@keyzer.com>
2024-01-27 17:58:27 -0600

    assessment=01 commit

---
+++ assessment-01/scripts/script.js
@@ -0,0 +1,31 @@
+function myFunction1()
+    {
+        (document.getElementById("changingText").innerHTML = "Changed from a function external file")
+    }
+
+    
+
+function myFunction2()
+    {
+        (document.getElementById("images").width = "300");
+    }
+
+
+    function myFunction3()
+    {
+        (document.getElementById("changingText").style.color = "purple");
+    }
+
+
+
+    function myFunction4()
+    {
+        (document.getElementById("images").style.display = "block");
+    }
+
+
+
+    function myFunction5()
+    {
+        (document.getElementById("images").style.display = "none");
+    }
